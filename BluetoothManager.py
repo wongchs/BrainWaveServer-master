f1c51aea5e8c82f712c48e3be3e8bf5d90855fae
Chyisiang <chyisiang8@gmail.com>
2024-09-20 21:31:48 +0800

    clean up

--- BluetoothManager.py
+++ BluetoothManager.py
@@ -1,5 +1,8 @@
 import asyncio
 from bleak import BleakScanner
+import bluetooth
+import time
+import json
 
 
 async def scan_ble_devices():
@@ -17,3 +20,43 @@ async def scan_ble_devices():
     return devices_list
 
 
+def start_bluetooth_server(board):
+    port = 4
+    uuid = "00001101-0000-1000-8000-00805F9B34FB"
+    phone_mac_address = "00:B5:D0:BB:45:30"
+    
+    try:
+        print(f"Searching for service on {phone_mac_address}...")
+        service_matches = bluetooth.find_service(uuid=uuid, address=phone_mac_address)
+        
+        if len(service_matches) == 0:
+            print("Couldn't find the specified service.")
+            return
+
+        first_match = service_matches[0]
+        port = first_match["port"]
+        name = first_match["name"]
+        host = first_match["host"]
+
+        print(f"Connecting to \"{name}\" on {host}")
+
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((host, port))
+        
+        data = board.get_current_board_data(20)
+        if data is not None and data.size > 0:
+            json_data = json.dumps(data.tolist())
+            try:
+                sock.send(json_data.encode())
+                
+                time.sleep(5)
+
+                print("Closing connection")
+                sock.close()
+            except Exception as e:
+                print(f"Error sending data: {e}")
+        
+    except bluetooth.BluetoothError as e:
+        print(f"Bluetooth Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
--- Main.py
+++ Main.py
@@ -1,59 +1,9 @@
 import asyncio
 import re
-import bluetooth
-import time
-import socket
-import json
 from Config import pattern as p
-from BluetoothManager import scan_ble_devices
+from BluetoothManager import scan_ble_devices, start_bluetooth_server
 from BrainWaveManager import connect_to_board, release_board, write_as_csv
 from PortManager import find_device
-
-def start_bluetooth_server(board):
-    port = 4
-    uuid = "00001101-0000-1000-8000-00805F9B34FB"
-    phone_mac_address = "00:B5:D0:BB:45:30"
-    
-    try:
-        print(f"Searching for service on {phone_mac_address}...")
-        service_matches = bluetooth.find_service(uuid=uuid, address=phone_mac_address)
-        
-        if len(service_matches) == 0:
-            print("Couldn't find the specified service.")
-            return
-
-        first_match = service_matches[0]
-        port = first_match["port"]
-        name = first_match["name"]
-        host = first_match["host"]
-
-        print(f"Connecting to \"{name}\" on {host}")
-
-        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        sock.connect((host, port))
-        
-        data = board.get_current_board_data(20)
-        if data is not None and data.size > 0:
-            json_data = json.dumps(data.tolist())
-            try:
-                sock.send(json_data.encode())
-                
-                time.sleep(5)
-
-                print("Closing connection")
-                sock.close()
-            except socket.error as e:
-                print(f"Error sending data: {e}")
-        
-    except bluetooth.BluetoothError as e:
-        print(f"Bluetooth Error: {e}")
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-
-    if port > 30:
-        print("Could not bind to any port. Exiting.")
-        return
-
 
 def main():
     port_list = find_device()
